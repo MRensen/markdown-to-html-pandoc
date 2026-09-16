92eddfd9cd03115746e546e795698cfacf6e9632
MRensen <markrensen@hotmail.com>
2026-09-16 13:51:17 +0200

    lua scriptje om target=_blank linkjes in de html te zetten

---
+++ external-links.lua
@@ -0,0 +1,9 @@
+function Link(el)
+  -- Only modify HTTP(S) links
+  if el.target:match("^https?://") then
+    el.attributes.target = "_blank"
+    el.attributes.rel = "noopener noreferrer"
+  end
+
+  return el
+end
--- markdown_to_html.ps1
+++ markdown_to_html.ps1
@@ -1,4 +1,4 @@
   Get-ChildItem -File -Filter *.md | ForEach-Object {
       $output = Join-Path .\html ($_.BaseName + ".html")
-      pandoc --from markdown+lists_without_preceding_blankline --standalone --syntax-highlighting monokai.theme --template=fragment-template.html  --to html --output $output $_.FullName
+      pandoc --from markdown+lists_without_preceding_blankline --standalone --lua-filter=external-links.lua --syntax-highlighting monokai.theme --template=fragment-template.html  --to html --output $output $_.FullName
   }
